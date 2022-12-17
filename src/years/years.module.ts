014f14a5751e2422d3931053141ee13867233abd
mi-cloud34 <62014097+mi-cloud34@users.noreply.github.com>
2022-12-17 15:59:20 +0300

    Delete src/years directory

--- src/years/years.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { YearsService } from './years.service';
-import { YearsController } from './years.controller';
-
-@Module({
-  controllers: [YearsController],
-  providers: [YearsService]
-})
-export class YearsModule {}
