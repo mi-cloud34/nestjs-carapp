65cc7caf858b54d2b5965b5600105b9a333065b8
Can Çevik <mazlumcancevik@gmail.com>
2022-12-18 13:34:53 +0300

    add setup swagger function

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,19 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSwagger } from './setup-swagger';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  // const configService = app.get<ConfigService>(ConfigService);
-  // const globalPrefix = configService.get('globalPrefix')
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-  //   cors: true
-  // })
-  /*  const appName = configService.get('appName')
-  const appDescription = configService.get('appDescription')
-  const apiVersion = configService.get('apiVersion')
-  const config = new DocumentBuilder()
-    .setTitle(appName)
-    .setDescription(appDescription)
-    .setVersion(apiVersion)
-    .build()
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
-  const document = SwaggerModule.createDocument(app, config)
+  const configService = app.get<ConfigService>(ConfigService);
 
-  SwaggerModule.setup('/', app, document)
-  SwaggerModule.setup(globalPrefix, app, document)
- 
- 
-  const port = configService.get('port') */
-  //await app.listen(port)
-  await app.listen(3000);
+  setupSwagger(app);
+
+  const port = configService.get('port');
+  await app.listen(port);
 }
 bootstrap();
